7fd4b08edf283f4113317fae276720ac8bb7cb7f
Rita Moura <ritamoura520@gmail.com>
2023-01-27 09:24:26 -0300

    Feat: implementa testes para cobrir 30% da aplicação

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from '../App';
-import { fetchAPI } from '../data/mock';
+import Provider from '../context/Provider';
+import Header from '../components/Header';
+import Table from '../components/Table';
+import SearchPlanet from '../components/SearchPlanets';
 
 describe('Testa se o componente Table funciona corretamente', () => {
   test('Testa se renderiza corretamente as infrmações', () => {
+    render(
+      <Provider>
+        <Header />
+        <SearchPlanet />
+        <Table />
+      </Provider>
+    )
   });
 
-  test('Testa se possui uma tabela', () => {
-    
-  })
 });
